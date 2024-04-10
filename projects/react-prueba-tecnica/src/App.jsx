8a6f54b3e17d979e5a67c3aeedb03ce10b105045
LenaaVN <leandro1531@outlook.es>
2024-04-10 09:59:14 +0200

    Reatc-practica terminada

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { useCatImage } from './hooks/useCatImage.js'
 import { useCatFact } from './hooks/useCatFact.js'
 
-export function App () {
+export function App() {
   const { fact, refreshFact } = useCatFact()
   const { imageUrl } = useCatImage({ fact })
 
@@ -14,10 +14,14 @@ export function App () {
     <main>
       <h1>App de gatitos</h1>
 
+      {fact && <p>{fact}</p>}
+
       <button onClick={handleClick}>Nuevo Fact</button>
 
-      {fact && <p>{fact}</p>}
-      {imageUrl && <img src={imageUrl} alt={`Imagen extraida usando las primeras tres palabras para ${fact}`} />}
+      <section>
+        {imageUrl && <img src={imageUrl} alt={`Imagen extraida usando las 
+      primeras tres palabras para ${fact}`} />}
+      </section>
     </main>
   )
 }
